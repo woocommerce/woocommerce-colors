dfae3778e88bb8b45cafd752d8b82f4b17110837
claudiosmweb <contato@claudiosmweb.com>
2014-11-18 22:19:49 -0200

    Created initial main file

---
+++ woocommerce-customizer.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Customizer
+ * Plugin URI: http://wordpress.org/plugins/woocommerce-customizer/
+ * Description: WooCommerce Customizer.
+ * Author: WooThemes
+ * Author URI: http://woothemes.com
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-customizer
+ * Domain Path: languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WC_Customizer' ) ) :
+
+/**
+ * WooCommerce Customizer main class.
+ */
+class WC_Customizer {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.3.0';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin.
+	 */
+	private function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Checks with WooCommerce is installed.
+		if ( class_exists( 'WooCommerce' ) && defined( 'WOOCOMMERCE_VERSION' ) && version_compare( WOOCOMMERCE_VERSION, '2.3', '>=' ) ) {
+			$this->includes();
+		} else {
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'woocommerce-customizer' );
+
+		load_textdomain( 'woocommerce-customizer', trailingslashit( WP_LANG_DIR ) . 'woocommerce-customizer/woocommerce-customizer-' . $locale . '.mo' );
+		load_plugin_textdomain( 'woocommerce-customizer', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Includes.
+	 *
+	 * @return void
+	 */
+	private function includes() {
+
+	}
+
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @return string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Customizer depends on the last version of %s to work!', 'woocommerce-customizer' ), '<a href="http://www.woothemes.com/woocommerce/" target="_blank">' . __( 'WooCommerce', 'woocommerce-customizer' ) . '</a>' ) . '</p></div>';
+	}
+}
+
+add_action( 'plugins_loaded', array( 'WC_Customizer', 'get_instance' ), 0 );
+
+endif;
